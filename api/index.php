0125e1ccdf778f44ec417aaea82897f6b19e2a65
Rafif <rafifmurtadho@gmail.com>
2023-07-20 16:34:09 +0800

    fix api

--- api/index.php
+++ api/index.php
@@ -3,8 +3,14 @@
 // Get the request URI
 $requestUri = $_SERVER['REQUEST_URI'];
 
-// Remove any leading '/api/' from the request URI
-$requestUri = preg_replace('/^\/api\//', '/', $requestUri);
+// Define the base URI for your API
+$baseUri = '/api';
+
+// Check if the request URI starts with the base URI
+if (strpos($requestUri, $baseUri) === 0) {
+    // Remove the base URI from the request URI
+    $requestUri = substr($requestUri, strlen($baseUri));
+}
 
 // Include the Laravel application's entry point
 require __DIR__ . '/../public/index.php';
