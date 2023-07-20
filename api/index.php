bc083c792de28625fc7139a91a0b0a98badd7117
Rafif <rafifmurtadho@gmail.com>
2023-07-20 16:32:06 +0800

    fix api

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-require __DIR__ . "/../public/index.php";
+// Get the request URI
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Remove any leading '/api/' from the request URI
+$requestUri = preg_replace('/^\/api\//', '/', $requestUri);
+
+// Include the Laravel application's entry point
+require __DIR__ . '/../public/index.php';
