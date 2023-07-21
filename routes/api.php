005b44caaa58323d3019974d073696756750c563
Rafif <rafifmurtadho@gmail.com>
2023-07-21 15:03:18 +0800

    fix api

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AgentController;
-use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
