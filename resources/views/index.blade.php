433b46142c18ef1502ce27202e33f3527e344605
Rafif <rafifmurtadho@gmail.com>
2023-07-24 13:54:23 +0800

    add smthg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -204,27 +204,14 @@
         <div class="container p-4 pb-0">
             <!-- Section: Social media -->
             <section class="mb-4">
-                <!-- Facebook -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #3b5998;" href="#!"
-                    role="button"><i class="fab fa-facebook-f"></i></a>
-
-                <!-- Twitter -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #55acee;" href="#!"
-                    role="button"><i class="fab fa-twitter"></i></a>
-
                 <!-- Google -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #dd4b39;" href="#!"
+                <a class="btn text-white btn-floating m-1" style="background-color: #dd4b39;" href="mailto:rafifmurtadho@gmail.com"
                     role="button"><i class="fab fa-google"></i></a>
-
-                <!-- Instagram -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" href="#!"
-                    role="button"><i class="fab fa-instagram"></i></a>
-
                 <!-- Linkedin -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #0082ca;" href="#!"
+                <a class="btn text-white btn-floating m-1" style="background-color: #0082ca;" href="https://www.linkedin.com/in/rafifmurtadho/"
                     role="button"><i class="fab fa-linkedin-in"></i></a>
                 <!-- Github -->
-                <a class="btn text-white btn-floating m-1" style="background-color: #333333;" href="#!"
+                <a class="btn text-white btn-floating m-1" style="background-color: #333333;" href="https://github.com/rafifrafael"
                     role="button"><i class="fab fa-github"></i></a>
             </section>
             <!-- Section: Social media -->
