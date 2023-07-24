4a30d37bbb1a3526e6daa0f69026ae5fa08deb21
Rafif <rafifmurtadho@gmail.com>
2023-07-25 00:19:05 +0800

    fix api

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AgentController;
+use App\Http\Controllers\API\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +19,24 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');   
+});
+
 Route::controller(AgentController::class)->group(function(){
     Route::resource('agents', AgentController::class);
     Route::get('agents/role/{role}', [AgentController::class, 'getByRole']);
     Route::get('agents/name/{name}', [AgentController::class, 'getByName']);
+});
+        
+Route::middleware('auth:sanctum')->group(function () {
+    //Route::resource('agents', AgentController::class);
+    Route::post('agents/add/', [AgentController::class, 'store']);
+    Route::post('agents/update/{id}', [AgentController::class, 'update']);
+    Route::delete('agents/delete/{id}', [AgentController::class, 'destroy']);
 });
